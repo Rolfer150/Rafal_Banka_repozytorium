459d6f3a973727ab5926e16b36598e134610ad2c
Rafal Banka <rbanka221@gmail.com>
2021-10-26 15:22:37 +0200

    2.0

--- main.cpp
+++ main.cpp
@@ -3,21 +3,12 @@ using namespace std;
 
 int main()
 {
-	int a, b;
-	float x0;
+	float a, b, x0;
 	cout << "Podaj wartosc 'a': ", cin >> a;
 	cout << "Podaj wartosc 'b': ", cin >> b;
 	
-	if(a != 0)
-	{
-		x0 = - b / a;
-		cout << "Miejsce zerowe to: " << x0;
-	}
-	
-	else
-	{
-		cout << "Wartosc 'a' jest rowna 0";
-	}
-	
+	x0 = - b / a;
+	cout << "Miejsce zerowe to: " << x0;
+		
 	return 0;
 }
