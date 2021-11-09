fc2b39bdfaca5b4a657a2674ebec81ee54014b22
Rafał Michalik <rafcik1771@gmail.com>
2021-11-09 14:13:30 +0100

    Initialize repository

--- main.cpp
+++ main.cpp
@@ -4,11 +4,20 @@ using namespace std;
 int main()
 {
 	float a, b, x0;
+	
 	cout << "Podaj wartosc 'a': ", cin >> a;
 	cout << "Podaj wartosc 'b': ", cin >> b;
 	
 	x0 = - b / a;
 	cout << "Miejsce zerowe to: " << x0;
+	
+	float A, B, C;
+	
+	cout << "Podaj wartosc 'A': ", cin >> A;
+	cout << "Podaj wartosc 'B': ", cin >> B;
+	cout << "Podaj wartosc 'C': ", cin >> C;
+	
+	if (A != 0)
 		
 	return 0;
 }
